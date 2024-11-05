ae4149773cdb2714f1313a65d611794b076989c7
Dimitris Stoikidis <jstoikidis@gmail.com>
2024-11-05 22:11:34 +0000

    Update main.go

--- main.go
+++ main.go
@@ -1,47 +1,59 @@
 package main
 
 import (
-	"fmt"
-	"github.com/St0iK/go-quote-parser/config"
-	"github.com/St0iK/go-quote-parser/dao"
-	"github.com/St0iK/go-quote-parser/parser"
-	"io/ioutil"
-	"log"
+    "fmt"
+    "github.com/St0iK/go-quote-parser/config"
+    "github.com/St0iK/go-quote-parser/dao"
+    "github.com/St0iK/go-quote-parser/parser"
+    "log"
+    "os"
 )
 
 var c config.Configuration
 
 func init() {
-	// Connect to the database
-	dao.Connect()
+    // Connect to the database
+    dao.Connect()
 
-	// register the parser factories
-	parser.Register("V1", parser.NewJsonFactory)
-	parser.Register("V2", parser.NewJsonFactory)
-	parser.Register("V3", parser.NewJsonFactory)
+    // Register the parser factories
+    parser.Register("V1", parser.NewJsonFactory)
+    parser.Register("V2", parser.NewJsonFactory)
+    parser.Register("V3", parser.NewJsonFactory)
 
-	c.GetConf()
+    // Load configuration
+    if err := c.GetConf(); err != nil {
+        log.Fatalf("Failed to load configuration: %v", err)
+    }
 }
 
 func main() {
-	log.Println("Initialising Go-Quote-Parser")
+    log.Println("Initializing Go-Quote-Parser")
 
-	log.Println("Reading all files inside quotes folder")
-	files, err := ioutil.ReadDir(c.QuotesFolder)
-	if err != nil {
-		log.Fatal(err)
-	}
+    log.Println("Reading all files inside quotes folder")
+    files, err := os.ReadDir(c.QuotesFolder)
+    if err != nil {
+        log.Fatalf("Failed to read directory %s: %v", c.QuotesFolder, err)
+    }
 
-	// loop through the files in the folder
-	for _, f := range files {
-		parserConfig := c.GetConfForFile(f.Name())
-		// crate a parser and pass the filename
-		p, _ := parser.GetParserForFile(parserConfig)
+    // Loop through the files in the folder
+    for _, f := range files {
+        parserConfig := c.GetConfForFile(f.Name())
 
-		// If parser was found for this file, then process it
-		if p != nil {
-			var res, _ = p.Process()
-			fmt.Println(res)
-		}
-	}
+        // Create a parser and pass the filename
+        p, err := parser.GetParserForFile(parserConfig)
+        if err != nil {
+            log.Printf("Failed to get parser for file %s: %v", f.Name(), err)
+            continue
+        }
+
+        // If parser was found for this file, process it
+        if p != nil {
+            res, err := p.Process()
+            if err != nil {
+                log.Printf("Error processing file %s: %v", f.Name(), err)
+                continue
+            }
+            fmt.Println(res)
+        }
+    }
 }
